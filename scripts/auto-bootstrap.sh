4438343f2aecbc69a7ed49a8fd89a46c9d072bf1
Guilherme Souza <57370623+guilhermefpv@users.noreply.github.com>
2025-12-17 23:42:38 -0300

    Enhance auto-bootstrap.sh with python3-venv and pipx
    Updated installation commands to include python3-venv and pipx for better environment management.

--- scripts/auto-bootstrap.sh
+++ scripts/auto-bootstrap.sh
@@ -40,10 +40,12 @@ sudo apt-get update
 sudo apt-get -y dist-upgrade
 
 # Install tools.
-sudo apt-get install -y make git python3 python3-pip
+# sudo apt-get install -y make git python3 python3-pip
+sudo apt-get install -y make git python3 python3-pip python3-venv pipx
 
 # Install Ansible via pip.
-sudo apt-get remove -y ansible
+# sudo apt-get remove -y ansible
+pipx install --include-deps ansible
 sudo apt-get autoremove -y
 python3 -m pip install --user ansible
 
